1b1420581375312ca4c03e846b3e28ea18d480dc
msut <marr994@o2.pl>
2016-11-15 09:53:08 +0100

    fix callack call

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import implementations.Callback;
 import implementations.SimpleLibrary;
 import interfaces.ICallback;
+import interfaces.IExitCallback;
 import interfaces.ILibrary;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -11,22 +13,49 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        IExitCallback exitCallback = (question) -> {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(question);
+            return "T".equalsIgnoreCase(scanner.nextLine());
+        };
 
+        start(exitCallback);
+    }
+
+    private static void start(IExitCallback exitCallback) {
+        try {
+            init(exitCallback);
+        } catch (NoSuchElementException ignored) {
+        } finally {
+            if (!exitCallback.getResult("Czy chcesz wyjsc z programu?")) {
+                start(exitCallback);
+            }
+        }
+    }
+
+    private static void init(IExitCallback exitCallback) {
+        Scanner scanner = new Scanner(System.in);
         String fileName;
         String successString;
         String failureString;
         ICallback callback = new Callback();
         ILibrary library = new SimpleLibrary();
-
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj nazwe pliku:");
-        fileName = scanner.nextLine();
+        fileName = readConsole(scanner);
         System.out.println("Podaj poprawny:");
-        successString = scanner.nextLine();
+        successString = readConsole(scanner);
         System.out.println("Podaj niepoprawny:");
-        failureString = scanner.nextLine();
+        failureString = readConsole(scanner);
+        if (fileName != null && successString != null && failureString != null) {
+            library.saveToFile(fileName, successString, failureString, callback, exitCallback);
+        }
+    }
 
-        library.saveToFile(fileName, successString, failureString, callback);
-        System.out.println("EXIT");
+    private static String readConsole(Scanner scanner) {
+        String text = scanner.nextLine();
+        if (text.equalsIgnoreCase("Q")) {
+            return null;
+        }
+        return text;
     }
 }
